5e194c99b7791e635b87fb4eb2f4f5a1b1ee4220
nericode <alanneri10@gmail.com>
2020-06-13 12:14:10 -0500

    add project

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var gplay = require("google-play-scraper");
+var store = require("app-store-scraper");
+
+app.get("/", function (request, response) {
+  if (request.query.os === "android") {
+    gplay
+      .app({ appId: request.query.appId, lang: "es", country: "mx" })
+      .then((r) => {
+        return response.send(r);
+      })
+      .catch((e) => {
+        return response.status(500).send(e);
+      });
+  } else {
+    store
+      .app({ id: request.query.appId, lang: "es", country: "mx" })
+      .then((r) => {
+        return response.send(r);
+      })
+      .catch((e) => {
+        return response.status(500).send(e);
+      });
+  }
+});
